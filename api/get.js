fa02229deeb274d268c06cddd06113be58ea399f
pitchiluxe <pitchiluxe@gmail.com>
2026-06-12 00:43:01 -0400

    fix: serve ZIP download (avoids Chrome Safe Browsing block on .exe)

--- api/get.js
+++ api/get.js
@@ -1,7 +1,7 @@
 export const config = { runtime: 'edge' }
 
 export default async function handler() {
-  const EXE = 'https://github.com/pitchiluxe/aihub-browser/releases/download/v1.0.0/AIHub-Browser-1.0.0-win-x64.exe'
+  const ZIP = 'https://github.com/pitchiluxe/aihub-browser/releases/download/v1.0.0/AIHub-Browser-1.0.0-win-x64.zip'
 
-  return Response.redirect(EXE, 302)
+  return Response.redirect(ZIP, 302)
 }
